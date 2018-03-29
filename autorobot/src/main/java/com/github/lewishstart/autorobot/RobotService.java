3168b7e8ca1958ada430267335de82f663c5e2e6
LewishStart <lewishstart@163.com>
2018-03-29 16:45:37 +0800

    自动加群做到加群OK

--- autorobot/src/main/java/com/github/lewishstart/autorobot/RobotService.java
+++ autorobot/src/main/java/com/github/lewishstart/autorobot/RobotService.java
@@ -5,6 +5,9 @@ import android.os.SystemClock;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+
 
 /**
  * author: sundong
@@ -13,15 +16,18 @@ import android.view.accessibility.AccessibilityEvent;
 
 public class RobotService extends BaseAccessibilityService {
     private static final String TAG = "RobotService";
-    private String action="";
+    private String action = "";
     private static final String LAUNCHER_UI_CLASS_NAME = "com.tencent.mm.ui.LauncherUI";
     private static final String SCAN_UI_CLASS_NAME = "com.tencent.mm.plugin.scanner.ui.BaseScanUI";
+
     private static final String AlbumPreviewUI = "com.tencent.mm.plugin.gallery.ui.AlbumPreviewUI";
     private static final String jiaqunUI = "com.tencent.mm.plugin.webview.ui.tools.WebViewUI";
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        action = intent.getStringExtra("action");
+        if(intent!=null) {
+            action = intent.getStringExtra("action");
+        }
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -40,20 +46,23 @@ public class RobotService extends BaseAccessibilityService {
                 break;
             case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED://窗口改变事件
                 Log.d(TAG, "eventType:TYPE_WINDOW_STATE_CHANGED");
-                if(action.equals("jiaqun")) {
+                if (action.equals("jiaqun")) {
                     if (LAUNCHER_UI_CLASS_NAME.equals(className)) {
-                        performViewClick(findViewByID("com.tencent.mm:id/g_"));
-                        SystemClock.sleep(200);
+                        SystemClock.sleep(2000);
+                        performViewClick(findViewByID("com.tencent.mm:id/g1"));
+                        SystemClock.sleep(2000);
                         performViewClick(findViewByText("扫一扫"));
                     } else if (SCAN_UI_CLASS_NAME.equals(className)) {
-                        performViewClick(findViewByID("com.tencent.mm:id/he"));
-                        SystemClock.sleep(200);
+                        SystemClock.sleep(2000);
+                        performViewClick(findViewByID("com.tencent.mm:id/h6"));
+                        SystemClock.sleep(2000);
                         performViewClick(findViewByText("从相册选取二维码"));
                     } else if (AlbumPreviewUI.equals(className)) {
-                        SystemClock.sleep(200);
-                        performViewClick(findViewByID("com.tencent.mm:id/cmq"));
-                    }else if(jiaqunUI.equals(className)) {
-
+                        SystemClock.sleep(2000);
+                        performViewClick(findViewByID("com.tencent.mm:id/f7"));
+                    } else if (jiaqunUI.equals(className)) {
+                        SystemClock.sleep(2000);
+                        execShellCmd("input tap 533 1020");
                     }
                 }
 
@@ -97,4 +106,23 @@ public class RobotService extends BaseAccessibilityService {
     public void onInterrupt() {
 
     }
+
+    private void execShellCmd(String cmd) {
+
+        try {
+            // 申请获取root权限，这一步很重要，不然会没有作用
+            Process process = Runtime.getRuntime().exec("su");
+            // 获取输出流
+            OutputStream outputStream = process.getOutputStream();
+            DataOutputStream dataOutputStream = new DataOutputStream(
+                    outputStream);
+            dataOutputStream.writeBytes(cmd);
+            dataOutputStream.flush();
+            dataOutputStream.close();
+            outputStream.close();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
 }
